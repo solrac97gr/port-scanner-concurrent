b86d726c479508f8f477c20795cc655a71cc07da
carlos97gr <carlosgperu97@gmail.com>
2022-07-25 21:04:47 +0300

    adding from, limit and usage file

--- main.go
+++ main.go
@@ -8,16 +8,20 @@ import (
 )
 
 var host *string
+var from *int
+var limit *int
 
 func init() {
-	host = flag.String("host", "scanme.nmap.org", "-h")
+	host = flag.String("host", "scanme.nmap.org", "hostname to scan")
+	from = flag.Int("from", 0, "from what port we will scan")
+	limit = flag.Int("limit", 65535, "until what port we will scan")
 }
 
 func main() {
 	flag.Parse()
-	fmt.Printf("Scanning %s\n", *host)
+	fmt.Printf("Scanning %s from port %d to %d\n", *host, *from, *limit)
 	var wg sync.WaitGroup
-	for i := 0; i < 65535; i++ {
+	for i := *from; i < *limit; i++ {
 		wg.Add(1)
 		go func(port int) {
 			defer wg.Done()
